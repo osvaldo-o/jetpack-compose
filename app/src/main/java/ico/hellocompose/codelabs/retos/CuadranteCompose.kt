e9c86ef32ce19cd441aa9a4bdc51518a3b65442b
osvaldo <osvaldo.guerra.c008@gmail.com>
2024-03-23 13:46:47 -0600

    colab cuadrante compose

---
+++ app/src/main/java/ico/hellocompose/codelabs/retos/CuadranteCompose.kt
@@ -0,0 +1,82 @@
+package ico.hellocompose.codelabs.retos
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewQuadrantsCompose() {
+    QuadrantsCompose()
+}
+
+@Composable
+fun QuadrantsCompose() {
+    Column(Modifier.fillMaxSize()) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)) {
+            QuadrantView(
+                title = "Text composable",
+                content = "Displays text and follows the recommended Material Design guidelines.",
+                color = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantView(
+                title = "Image composable",
+                content = "Creates a composable that lays out and draws a given Painter class object.",
+                color = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)) {
+            QuadrantView(
+                title = "Row composable",
+                content = "A layout composable that places its children in a horizontal sequence.",
+                color = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantView(
+                title = "Column composable",
+                content = "A layout composable that places its children in a vertical sequence.",
+                color = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantView(title: String, content: String, color : Color, modifier: Modifier) {
+    Box(modifier = modifier.background(color)) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+            Text(text = content, textAlign = TextAlign.Justify)
+        }
+    }
+}
